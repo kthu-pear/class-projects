67d1eaf257668c5ad0323c0cd6938788f09755f7
asmallpear <lekhanhthu010902@gmail.com>
2026-03-31 22:16:32 +0700

    Update 3-weatherObject

---
+++ 3-weatherObject.js
@@ -0,0 +1,8 @@
+const currentWeather = {
+    temperature: 30,
+    conditions: 'sunny',
+};
+
+console.log(currentWeather.temperature);
+console.log(`The temperature is ${currentWeather.temperature} degrees C and the weather is ${currentWeather.conditions}.`)
+
